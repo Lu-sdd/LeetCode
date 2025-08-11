192147e2595c4070ae6db28bfef2b485ecb2a6cb
Lu <114335706+Lu-sdd@users.noreply.github.com>
2025-08-11 18:39:15 +0100

    Time: 0 ms (100%), Space: 40.3 MB (65.67%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+
+        int prev1 = 2;
+        int prev2 = 1;
+        int cur = 0;
+        for(int i = 3; i <= n; i++){
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return cur;
+    }
+}
